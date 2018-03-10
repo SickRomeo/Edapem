f3ad79355aaa8346d8aac3d19397f6f1f88097d2
Al Amin <007.alamin@gmail.com>
2018-03-11 00:28:06 +0700

    add views structure

---
+++ mvc/views/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			<?php isset($pagetitle) ? $pagetitle : 'EDAPEM';?>
+		</title>
+	</head>
+	<body>
+		<?php include('header.php');?>
+
+		<?php include('footer.php');?>
+	</body>
+</html>
